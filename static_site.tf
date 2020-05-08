ea7fc27903d1feb02623c1010143447098d6f787
Maël Valais <mael.valais@gmail.com>
2020-05-08 17:51:09 +0200

    tf: more context around the need for tfstate-sa to have write priv

--- static_site.tf
+++ static_site.tf
@@ -9,6 +9,18 @@ provider "google" {
   region  = "us-central1"
 }
 
+# IMPORTANT: the CI's service account "tfstate-sa" must be able to access
+# to gs://tfstate-gh, but since it is also used to create the
+# gs://my-static-site bucket, we also have to give it write privileges
+# using:
+#
+#    gcloud projects add-iam-policy-binding tfstate750 \
+#        --role=roles/storage.admin \
+#        --member=serviceAccount:tfstate-sa@tfstate750.iam.gserviceaccount.com
+#
+# I wished I could also create that binding here in the terraform config
+# file, but there seems to be an egg-and-chicken issue here...
+
 resource "google_storage_bucket" "static_site" {
   name          = "my-static-site"
   location      = "EU"
@@ -27,4 +39,10 @@ resource "google_storage_bucket_object" "index_html" {
   bucket  = google_storage_bucket.static_site.name
   name    = "index.html"
   content = "😅"
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  bucket = google_storage_bucket.static_site.name
+  role   = "roles/storage.admin"
+  member = "tfstate-sa@tfstate750.iam.gserviceaccount.com"
 }
